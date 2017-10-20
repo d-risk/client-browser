30365b5a2264fc5842d95fe9d8e82d6a01f82a01
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-20 15:57:19 +0800

    refactor log-in to login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppComponent} from './app.component';
 import {AboutComponent} from './about/about.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {HomeComponent} from './home/home.component';
-import {LogInComponent} from './log-in/log-in.component';
+import {LoginComponent} from './login/login.component';
 import {LogOutComponent} from './log-out/log-out.component';
 import {NavigationComponent} from './navigation/navigation.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
@@ -18,7 +18,7 @@ import {RoutingModule} from './routing/routing.module';
     AboutComponent,
     DashboardComponent,
     HomeComponent,
-    LogInComponent,
+    LoginComponent,
     LogOutComponent,
     NavigationComponent,
     SignUpComponent,
