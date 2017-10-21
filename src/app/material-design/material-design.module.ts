d64e8e09bfb020e0ac92a0de7c4d92258c73508b
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-21 14:01:25 +0800

    add angular material design progress spinner module

--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -7,6 +7,7 @@ import {
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
+  MatProgressSpinnerModule,
   MatSidenavModule,
   MatToolbarModule
 } from '@angular/material';
@@ -20,6 +21,7 @@ import {
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule,
   ],
@@ -31,6 +33,7 @@ import {
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule,
   ],
