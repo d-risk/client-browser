ba7216275716983976d8f41ff85a8f76a172477a
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-21 15:14:51 +0800

    add routing for dashboard

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {AboutComponent} from '../about/about.component';
+import {DashboardComponent} from '../dashboard/dashboard.component';
 import {HomeComponent} from '../home/home.component';
 import {LoginComponent} from '../login/login.component';
 import {LogoutComponent} from '../logout/logout.component';
 
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
+  {path: 'dashboard', component: DashboardComponent},
   {path: 'login', component: LoginComponent},
   {path: 'logout', component: LogoutComponent},
   {path: '', component: HomeComponent},
