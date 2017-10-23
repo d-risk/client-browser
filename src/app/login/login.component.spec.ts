6f00b0c75a7c25b3c6a31096a6d3535dd3076b37
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-23 15:09:19 +0800

    fix login component test setup (incomplete)

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,16 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatProgressSpinnerModule} from '@angular/material';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
+import {MaterialDesignModule} from '../material-design/material-design.module';
+import {AuthenticationService} from '../authentication.service';
+import {RouterModule, Routes} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {SignUpComponent} from '../sign-up/sign-up.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([
+          {path: '/reset', component: LoginComponent},
+          {path: '/signup', component: SignUpComponent},
+        ]),
+
+        MatCardModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatProgressSpinnerModule
+      ],
+      providers: [
+        {provides: AuthenticationService, useClass: AuthenticationService},
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
