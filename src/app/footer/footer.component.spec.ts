cd64ced2d9cee31b630ffa2e9d74e40ff744e9a4
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-23 10:08:44 +0800

    fix tests for footer component

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatCardModule} from '@angular/material';
 
-import { FooterComponent } from './footer.component';
+import {FooterComponent} from './footer.component';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,9 +9,10 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [FooterComponent],
+      imports: [MatCardModule],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
