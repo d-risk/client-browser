d2581e4aa78c863e42aaa199ae7b1668e1892196
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-23 17:31:34 +0800

    update app component to use the modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,23 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
-import {AboutComponent} from './components/about/about.component';
-import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {MarketingComponent} from './components/marketing/marketing.component';
-import {LoginComponent} from './components/login/login.component';
-import {LogoutComponent} from './components/logout/logout.component';
-import {NavigationComponent} from './components/navigation/navigation.component';
-import {SignUpComponent} from './components/sign-up/sign-up.component';
 
-import {RoutingModule} from './routing/routing.module';
-import {MaterialDesignModule} from './material-design/material-design.module';
-
-import {AuthenticationService} from './authentication.service';
-import {UserService} from './user.service';
+import {AppRoutingModule} from './app-routing.module';
+import {NavigationModule} from './components/navigation/navigation.module';
+import {FooterModule} from './components/footer/footer.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-    AboutComponent,
-    DashboardComponent,
-    FooterComponent,
-    MarketingComponent,
-    LoginComponent,
-    LogoutComponent,
-    NavigationComponent,
-    SignUpComponent,
-  ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    ReactiveFormsModule,
 
-    RoutingModule,
-    MaterialDesignModule,
+    FooterModule,
+    NavigationModule,
+
+    AppRoutingModule,
   ],
-  providers: [
-    AuthenticationService,
-    UserService
-  ],
+  declarations: [AppComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
