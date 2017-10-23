0d8d57fcc55d1b0afde3922d7ed54fa1d11bf88c
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-23 17:02:15 +0800

    add import for missing `MatCardModule`

--- src/app/components/logout/logout.module.ts
+++ src/app/components/logout/logout.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material';
 
 import {LogoutComponent} from './logout.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatCardModule,
   ],
   declarations: [LogoutComponent]
 })
