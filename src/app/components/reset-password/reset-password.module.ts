d5680b69de96217dd40370c9fb78465b1caeba96
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-23 17:23:06 +0800

    add reset password module

---
+++ src/app/components/reset-password/reset-password.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {ResetPasswordComponent} from './reset-password.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ResetPasswordComponent]
+})
+export class ResetPasswordModule {
+}
