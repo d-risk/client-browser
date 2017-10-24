b6652e9e7a41221e335a8dac879985ca75a2dc61
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-24 20:54:21 +0800

    fix failing test case for navigation module

--- src/app/modules/navigation/navigation.component.spec.ts
+++ src/app/modules/navigation/navigation.component.spec.ts
@@ -1,16 +1,44 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import {RouterTestingModule} from '@angular/router/testing';
 
-import { NavigationComponent } from './navigation.component';
+import {NavigationComponent} from './navigation.component';
+
+import {AuthenticationService} from '../../services/authentication.service';
+
+import {createMockAuthenticationService, MockComponent} from '../../../testing/mock-stub';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
+  let mockAuthenticationService;
 
   beforeEach(async(() => {
+    mockAuthenticationService = createMockAuthenticationService();
+
     TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
+      imports: [
+        HttpClientTestingModule,
+
+        MatButtonModule,
+        MatIconModule,
+        MatToolbarModule,
+
+        RouterTestingModule.withRoutes([
+          {path: 'about', component: MockComponent},
+          {path: 'dashboard', component: MockComponent},
+          {path: 'login', component: MockComponent},
+          {path: 'logout', component: MockComponent},
+          {path: '', component: MockComponent},
+        ]),
+      ],
+      declarations: [NavigationComponent],
+      providers: [
+        {provide: AuthenticationService, useValue: mockAuthenticationService},
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
