01e3b94cbb694a975f2399e4fdd0ea60b3526b6c
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-26 18:19:56 +0800

    add the `company-name-search` and`company-report-search` modules to the `dashboard` module

--- src/app/modules/dashboard/dashboard.component.spec.ts
+++ src/app/modules/dashboard/dashboard.component.spec.ts
@@ -1,16 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatAutocompleteModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTabsModule} from '@angular/material';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
 
-import { DashboardComponent } from './dashboard.component';
+import {DashboardComponent} from './dashboard.component';
+import {CompanyNameSearchService} from './company-name-search/company-name-search.service';
+import {createMockComponent, FakeComponent} from '../../../testing/mock-stub.spec';
+import {createMockCompanyNameSearchService} from '../../../testing/mock-stub.spec';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let mockCompanySearchService: CompanyNameSearchService;
 
   beforeEach(async(() => {
+    mockCompanySearchService = createMockCompanyNameSearchService();
+
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      imports: [
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+
+        MatAutocompleteModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatTabsModule,
+
+        NgxChartsModule,
+      ],
+      declarations: [
+        DashboardComponent,
+        createMockComponent({selector: 'app-company-name-search'}),
+        createMockComponent({selector: 'app-company-report-search'}),
+      ],
+      providers: [
+        {provide: CompanyNameSearchService, useValue: mockCompanySearchService}
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,13 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material';
 
 import {DashboardComponent} from './dashboard.component';
+import {CompanyNameSearchModule} from './company-name-search/company-name-search.module';
+import {CompanyReportSearchModule} from './company-report-search/company-report-search.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatCardModule,
+
+    CompanyNameSearchModule,
+    CompanyReportSearchModule,
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent],
 })
 export class DashboardModule {
 }
