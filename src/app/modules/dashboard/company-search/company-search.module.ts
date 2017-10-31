7be32702c932c95a6c5228fd6d781fefdc2ae96b
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-10-31 19:49:47 +0800

    convert icon into a clickable button

--- src/app/modules/dashboard/company-search/company-search.module.ts
+++ src/app/modules/dashboard/company-search/company-search.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HttpModule} from '@angular/http';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatAutocompleteModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
+import {MatAutocompleteModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
 
 import {CompanySearchComponent} from './company-search.component';
 import {CompanyNameSearchService} from './company-name-search.service';
@@ -11,10 +12,12 @@ import {CompanyReportSearchService} from './company-report-search.service';
 @NgModule({
   imports: [
     CommonModule,
+    HttpModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
 
     MatAutocompleteModule,
+    MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
     MatIconModule,
