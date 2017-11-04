80389e1c82b0f2f92930dad52b1d9e91fbd5bfb7
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-11-04 17:14:06 +0800

    add some necessary info to the footer

--- src/app/modules/footer/footer.component.ts
+++ src/app/modules/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  year(): string {
+    const year = new Date().getFullYear();
+    return year > 2017 ? `2017 - ${year}` : '2017';
   }
 
 }
