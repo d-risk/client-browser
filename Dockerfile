95e88285f01cfaf358d04a2853ed54350f9d1698
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-11-07 01:19:51 +0800

    use sed to change server_name and remove user

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM nginx:alpine
 
 RUN rm -rf /usr/share/nginx/html/*
+COPY ./nginx/start-nginx.sh /
 COPY ./dist /usr/share/nginx/html
 
 CMD ["./start-nginx.sh"]
--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 sed -i "s/listen\(\s\+\)\d\+;/listen\1${PORT};/" /etc/nginx/conf.d/default.conf
+sed -i "s/server_name\(\s\+\)localhost;/server_name\1_;/" /etc/nginx/conf.d/default.conf
+sed -i "s/user\(\s\+\)nginx;//" /etc/nginx/nginx.conf
+
+cat /etc/nginx/conf.d/default.conf
+
 nginx -g "daemon off;"
