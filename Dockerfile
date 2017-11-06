422fa0639b0611c71df34109949db56bccde8573
Christopher Zhong <chriszhong@users.noreply.github.com>
2017-11-07 01:31:26 +0800

    clean up script and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,3 @@ COPY ./nginx/start-nginx.sh /
 COPY ./dist /usr/share/nginx/html
 
 CMD ["./start-nginx.sh"]
-#CMD ["sh", "-c", "envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"]
-#CMD ["nginx", "-g", "daemon off;"]
--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+FILE_DEFAULT=/etc/nginx/conf.d/default.conf
+FILE_NGINX=/etc/nginx/nginx.conf
 
-sed -i "s/listen\(\s\+\)\d\+;/listen\1${PORT};/" /etc/nginx/conf.d/default.conf
-sed -i "s/server_name\(\s\+\)localhost;/server_name\1_;/" /etc/nginx/conf.d/default.conf
-sed -i "s/user\(\s\+\)nginx;//" /etc/nginx/nginx.conf
+echo Updating listening port to ${PORT}
+sed -i "s/listen\(\s\+\)\d\+;/listen\1${PORT};/" ${FILE_DEFAULT}
+sed -i "s/server_name\(\s\+\)localhost;/server_name\1_;/" ${FILE_DEFAULT}
+sed -i "s/user\(\s\+\)nginx;//" ${FILE_NGINX}
 
-cat /etc/nginx/conf.d/default.conf
+echo Contents of ${FILE_DEFAULT}
+cat ${FILE_DEFAULT}
+echo Contents of ${FILE_NGINX}
+cat ${FILE_DEFAULT}
 
 nginx -g "daemon off;"
