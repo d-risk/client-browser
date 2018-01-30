ae1bd8a8389c9704e57d4a86fcfd3b336b5c16dc
Christopher Zhong <1946052+chriszhong@users.noreply.github.com>
2018-01-30 18:32:51 +0800

    login component redirects to dashboard if authentication is successful, otherwise to the root

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+
+import {AuthenticationService} from "../../authentication/authentication.service";
 
 @Component({
   // selector: 'app-login',
@@ -7,9 +10,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
   }
 
   ngOnInit() {
+    setTimeout(() => {
+      console.log('login: authenticated: ' + this.authenticationService.isAuthenticated());
+      if (this.authenticationService.isAuthenticated()) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.router.navigate([''])
+      }
+    }, 2000);
   }
 }
