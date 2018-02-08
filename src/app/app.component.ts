1d91f9561657e1ef63cf6dffb74816778ae8c81b
Christopher Zhong <1946052+chriszhong@users.noreply.github.com>
2018-02-08 13:12:39 +0800

    add page title to configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
+
+import {AppConfig} from "./configuration/app.config";
 import {AuthenticationService} from './authentication/authentication.service';
 
 @Component({
@@ -7,10 +9,14 @@ import {AuthenticationService} from './authentication/authentication.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private title: Title, private authenticationService: AuthenticationService) {
-    this.title.setTitle('D-Risk');
-    authenticationService.handleAuthentication();
+  constructor(private title: Title, private config: AppConfig, private authenticationService: AuthenticationService) {
   }
+
+  ngOnInit() {
+    this.title.setTitle(this.config.get("title"));
+    this.authenticationService.handleAuthentication();
+  }
+
 }
