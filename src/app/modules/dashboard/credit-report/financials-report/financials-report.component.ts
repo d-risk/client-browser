e3e9ed345bb0e2ba9854c3a94d96ca94cfe460c7
Christopher Zhong <1946052+chriszhong@users.noreply.github.com>
2018-03-19 21:22:30 +0800

    convert plain table to angular material table

--- src/app/modules/dashboard/credit-report/financials-report/financials-report.component.ts
+++ src/app/modules/dashboard/credit-report/financials-report/financials-report.component.ts
@@ -1,55 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 import {FinancialsReport} from "../../../../credit-report/credit-report";
-import {RiskDriverData} from "./risk-driver/risk-driver.component";
 
 @Component({
   selector: 'app-financials-report',
   templateUrl: './financials-report.component.html',
   styleUrls: ['./financials-report.component.css']
 })
-export class FinancialsReportComponent implements OnInit {
+export class FinancialsReportComponent {
 
   @Input() financialsReports: FinancialsReport[];
-
-  riskDriverData: RiskDriverData;
-
-  ngOnInit(): void {
-    let riskDriversMap = new Map<string, Map<string, Map<Date, number>>>();
-
-    let dates = new Set<Date>();
-    let riskDriverCategories = new Set<string>();
-    let riskDriverNames = new Set<string>();
-
-    this.financialsReports.forEach(report => {
-        dates.add(report.financialsReportDate);
-        report.riskDrivers.forEach(riskDriver => {
-          riskDriverCategories.add(riskDriver.category);
-          let namesMap = FinancialsReportComponent.getOrSet(riskDriversMap, riskDriver.category, Map);
-          riskDriver.numbers.forEach(value => {
-            riskDriverNames.add(value.name);
-            let numbersMap = FinancialsReportComponent.getOrSet(namesMap, value.name, Map);
-            numbersMap.set(report.financialsReportDate, value.value);
-          })
-        });
-      }
-    );
-    this.riskDriverData = {
-      dates: Array.from(dates.keys()).sort(),
-      categories: Array.from(riskDriverCategories.keys()).sort(),
-      names: Array.from(riskDriverNames.keys()).sort(),
-      numbers: riskDriversMap,
-    };
-  }
-
-
-  private static getOrSet<K, V>(map: Map<K, V>, key: K, DefaultValue: { new(): V }): V {
-    let value = map.get(key);
-    if (!value) {
-      value = new DefaultValue();
-      map.set(key, value);
-    }
-    return value;
-  }
 
 }
--- src/app/modules/dashboard/credit-report/financials-report/risk-driver/risk-driver.component.ts
+++ src/app/modules/dashboard/credit-report/financials-report/risk-driver/risk-driver.component.ts
@@ -1,39 +1,56 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
-export interface RiskDriverData {
-  dates: Date[]
-  categories: string[]
-  names: string[]
-  numbers: Map<string, Map<string, Map<Date, number>>>
-}
+import {FinancialsReport} from "../../../../../credit-report/credit-report";
 
 @Component({
   selector: 'app-risk-driver',
   templateUrl: './risk-driver.component.html',
   styleUrls: ['./risk-driver.component.css']
 })
-export class RiskDriverComponent {
+export class RiskDriverComponent implements OnInit {
 
-  @Input() data: RiskDriverData;
+  @Input() financialsReports: FinancialsReport[];
 
-  dates(): Date[] {
-    return this.data.dates
-  }
+  columnsOrder: string[] = ['name'];
+  dates: Date[];
+  dataSource: MatTableData[] = [];
 
-  categories(): string[] {
-    return this.data.categories
-  }
+  ngOnInit() {
+    let dates = new Set<Date>();
 
-  names(): string[] {
-    return this.data.names
-  }
+    this.financialsReports.forEach(report => {
+        dates.add(report.financialsReportDate);
+        report.riskDrivers.forEach(riskDriver => {
+          let data = this.dataSource.find(value => value.category.localeCompare(riskDriver.category) === 0);
+          if (!data) {
+            data = {category: riskDriver.category, names: new Map()};
+            this.dataSource.push(data);
+          }
+          riskDriver.numbers.forEach(value => getOrSet(data.names, value.name, Map).set(report.financialsReportDate, value.value));
+        });
+      }
+    );
 
-  number(category: string, name: string, date: Date): number {
-    return this.data.numbers.get(category).get(name).get(date)
+    this.dates = Array.from(dates.keys()).sort();
+    this.dates.forEach(value => this.columnsOrder.push(String(value)));
   }
 
   formatDate(date: Date) {
     return new Date(date).toLocaleDateString()
   }
 
+}
+
+interface MatTableData {
+  category: string
+  names: Map<string, Map<Date, number>>
+}
+
+function getOrSet<K, V>(map: Map<K, V>, key: K, Type: { new(): V }): V {
+  let value = map.get(key);
+  if (!value) {
+    value = new Type();
+    map.set(key, value);
+  }
+  return value;
 }
